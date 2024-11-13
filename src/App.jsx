f176e4510c27ac175cdeb063688671a26ae3815a
HongJeong1113 <94837066+HongJeong1113@users.noreply.github.com>
2024-11-13 23:33:03 +0900

    Revert "feature/all-page"
    This reverts commit 7d6ff680f239689910906915aef39efbdc70e726.

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,27 @@
 import "./styles.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { MainScreenVer2 } from "./MainScreenVer2/MainScreenVer2";
+
 import { LoginVer2 } from "./LoginVer2/LoginVer2";
-import { Component } from "./Component/Component";
-import { PhotoEvaluation } from "./PhotoEvaluation/PhotoEvaluation";
-import { Result } from "./Result/Result";
-import { Modify } from "./Modify/Modify";
-import {Lecture} from "./Lecture/Lecture";
-import {Detail} from "./Detail/Detail";
-import {Album} from "./Album/Album";
-import { useState } from "react";
-import ToggleMenu from "./ToggleMenu/ToggleMenu";
 
-function App() {
-  const [isMenuOpen, setMenuOpen] = useState(false);
+/*import { BrowserRouter as Router, Routes, Route } from "react-router-dom";*/
 
-  const toggleMenu = () => {
-    setMenuOpen(!isMenuOpen);
-  };
-
+export default function App() {
   return (
-    <Router>
-      <div className="app">
-        {!isMenuOpen && (
-          <div className="hamburger_icon" onClick={toggleMenu}>
-            <img src="hamburger_icon.png"/>
-          </div>
-        )}
-
-        {/* 사이드 메뉴 */}
-        <ToggleMenu isOpen={isMenuOpen} toggleMenu={toggleMenu} />
-
-        <Routes>
-          <Route path="/" element={<MainScreenVer2 />} />
-          <Route path="/Login" element={<LoginVer2 />} />
-          <Route path="/Component" element={<Component />} />
-          <Route path="/PhotoEvaluation" element={<PhotoEvaluation />} />
-          <Route path="/Result" element={<Result />} />
-          <Route path="/Album" element={<Album />} />
-          <Route path="/Modify" element={<Modify />} />
-          <Route path="/Lecture" element={<Lecture />} />
-          <Route path="/Detail" element={<Detail />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <LoginVer2 />
+    </div>
   );
 }
 
+/*
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={< LoginVer2 />} />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
+}
 export default App;
+*/
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000, //로컬 호스트 주소 변경
-  },
 })
